b6c323fc3cd830e9077227585559f3801fb42bb5
dskrenta <dskrenta@gmail.com>
2016-08-15 03:04:13 -0700

    c style editor config

--- hello.c
+++ hello.c
@@ -3,16 +3,34 @@
 #define WIDTH 10
 #define LENGTH 234
 
+void reverseArray(int arr[], int start, int end)
+{
+    int temp;
+    while(start < end)
+    {
+      temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--:
+    }
+}
+
+int add(int num1, int num2) 
+{
+    return num1 + num2;
+}
+
 int main()
 {
-  int area;
-  area = WIDTH * LENGTH;
+    int area;
+    area = WIDTH * LENGTH;
 
-  printf("The value of the area is: %d", area);
+    printf("The value of the area is: %d", area);
 
-  int num = 5;
-  printf("%i \n", num);
-  printf("Hello, World! \n");
+    int num = 5;
+    printf("%i \n", num);
+    printf("Hello, World! \n");
 
-  return 0;
+    return 0;
 }
