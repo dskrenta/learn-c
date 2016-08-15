0f0d245c59e7835339018cc029f9d38b9ba2974e
dskrenta <dskrenta@gmail.com>
2016-08-15 02:51:14 -0700

    constants example

--- hello.c
+++ hello.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main() {
+#define WIDTH 10
+#define LENGTH 234
+
+int main()
+{
+  int area;
+  area = WIDTH * LENGTH;
+
+  printf("The value of the area is: %d", area);
+
+  int num = 5;
+  printf("%i \n", num);
   printf("Hello, World! \n");
-  
+
   return 0;
 }
